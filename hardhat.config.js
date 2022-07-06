6020593e6f6b5ea477bcd69b1ab21e6a9c910094
Nedelin Slavov <nedelin.slavov@gmail.com>
2022-07-06 17:54:54 +0300

    Updated hardhat configuration for rinkeby network

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,9 +11,7 @@ require("hardhat-deploy")
  */
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
-const RINKEBY_RPC_URL =
-    process.env.RINKEBY_RPC_URL ||
-    "https://eth-rinkeby.alchemyapi.io/v2/xQ5W0YsYUcwLh4ADbXQXo-AzBhxnT8p"
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 const REPORT_GAS = process.env.REPORT_GAS || false
